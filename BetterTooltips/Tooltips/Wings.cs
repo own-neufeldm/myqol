7234ff4f8f482c8f5d238d56b69f74467c9c5c89
Martin Neufeld <m.neufeld946@gmail.com>
2026-06-29 20:51:23 +0200

    Comply with OCD

--- BetterTooltips/Tooltips/Wings.cs
+++ BetterTooltips/Tooltips/Wings.cs
@@ -8,39 +8,12 @@ namespace BetterTooltips.Tooltips
 {
   public class Wings : GlobalItem
   {
-    // todo: abstract
-    private static bool IsTarget(Item item)
-    {
-      return item.wingSlot > 0;
-    }
-
-    // todo: abstract
-    private static bool PlayerIsHoveringSocialSlot()
-    {
-      return Main.HoverItem.social;
-    }
-
-    // todo: abstract
-    private static string GetComparisonText(float hoveredValue, float equippedValue)
-    {
-      return $"[c/{GetComparisonColorHex(hoveredValue, equippedValue)}:{equippedValue:0.##}]";
-    }
-
-    // todo: abstract
-    private static string GetComparisonColorHex<T>(T hoveredValue, T equippedValue)
-      where T : IComparisonOperators<T, T, bool>
-    {
-      if (hoveredValue < equippedValue) return "FF0000"; // red
-      if (hoveredValue > equippedValue) return "00FF00"; // green
-      return "FFFF00";                                   // yellow
-    }
-
     public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
     {
-      if (!IsTarget(item) || PlayerIsHoveringSocialSlot()) return;
+      if (!IsWing(item) || Utils.IsHoveringSocialSlot()) return;
 
-      var player = Main.LocalPlayer;
-      var hoveredStats = player.GetWingStats(item.wingSlot);
+      Player player = Main.LocalPlayer;
+      WingStats hoveredStats = player.GetWingStats(item.wingSlot);
 
       WingStats? equippedStats =
         player.equippedWings != null && player.equippedWings.type != item.type
@@ -51,28 +24,9 @@ namespace BetterTooltips.Tooltips
       tooltips.Add(GetHorizontalAccelerationLine(hoveredStats, equippedStats));
     }
 
-    private TooltipLine GetFlightTimeLine(WingStats hoveredStats, WingStats? equippedStats)
+    private static bool IsWing(Item item)
     {
-      float hoveredValue = GetFlightTimeInSeconds(hoveredStats);
-      string text = $"Flight time: {hoveredValue:0.##} seconds";
-      if (equippedStats != null)
-      {
-        float equippedValue = GetFlightTimeInSeconds((WingStats)equippedStats);
-        text += $" ({GetComparisonText(hoveredValue, equippedValue)})";
-      }
-      return new(Mod, "WingFlightTime", text);
-    }
-
-    private TooltipLine GetHorizontalAccelerationLine(WingStats hoveredStats, WingStats? equippedStats)
-    {
-      float hoveredValue = GetHorizontalAccelerationInPercent(hoveredStats);
-      string text = $"Horizontal acceleration: +{hoveredValue}%";
-      if (equippedStats != null)
-      {
-        float equippedValue = GetHorizontalAccelerationInPercent((WingStats)equippedStats);
-        text += $" ({GetComparisonText(hoveredValue, equippedValue)})";
-      }
-      return new(Mod, "WingHorizontalAcceleration", text);
+      return item.wingSlot > 0;
     }
 
     private static float GetFlightTimeInSeconds(WingStats stats)
@@ -80,9 +34,58 @@ namespace BetterTooltips.Tooltips
       return stats.FlyTime / 60f;
     }
 
-    private static int GetHorizontalAccelerationInPercent(WingStats stats)
+    private static int GetHorizontalAccelerationBonusInPercent(WingStats stats)
     {
-      return (int)(stats.AccRunAccelerationMult - 1f) * 100;
+      return (int)((stats.AccRunAccelerationMult - 1f) * 100);
+    }
+
+    private TooltipLine GetFlightTimeLine(WingStats hoveredStats, WingStats? equippedStats)
+    {
+      float hoveredValue = GetFlightTimeInSeconds(hoveredStats);
+      string text = $"Flight time: {hoveredValue:0.##} seconds";
+
+      if (equippedStats.HasValue)
+      {
+        float equippedValue = GetFlightTimeInSeconds(equippedStats.Value);
+        text += $" ({Utils.GetComparisonText(hoveredValue, equippedValue)})";
+      }
+
+      return new TooltipLine(Mod, "WingFlightTime", text);
+    }
+
+    private TooltipLine GetHorizontalAccelerationLine(WingStats hoveredStats, WingStats? equippedStats)
+    {
+      float hoveredValue = GetHorizontalAccelerationBonusInPercent(hoveredStats);
+      string text = $"Horizontal acceleration: +{hoveredValue}%";
+
+      if (equippedStats.HasValue)
+      {
+        float equippedValue = GetHorizontalAccelerationBonusInPercent(equippedStats.Value);
+        text += $" ({Utils.GetComparisonText(hoveredValue, equippedValue)})";
+      }
+
+      return new TooltipLine(Mod, "WingHorizontalAcceleration", text);
+    }
+  }
+
+  public static class Utils
+  {
+    public static bool IsHoveringSocialSlot()
+    {
+      return Main.HoverItem.social;
+    }
+
+    public static string GetComparisonText(float hoveredValue, float equippedValue)
+    {
+      return $"[c/{GetComparisonColorHex(hoveredValue, equippedValue)}:{equippedValue:0.##}]";
+    }
+
+    public static string GetComparisonColorHex<T>(T hoveredValue, T equippedValue)
+      where T : IComparisonOperators<T, T, bool>
+    {
+      if (hoveredValue < equippedValue) return "FF0000"; // red
+      if (hoveredValue > equippedValue) return "00FF00"; // green
+      return "FFFF00";                                   // yellow
     }
   }
 }
